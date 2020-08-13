c8bea68fd40f11c5b239bf566be55efaad9ec172
Franz Neulist Carroll <franzneulistcarroll@gmail.com>
2020-08-13 12:20:56 +1000

    BBands

--- sql/dashboards.sql
+++ sql/dashboards.sql
@@ -14,12 +14,46 @@ WITH candles AS (
 )
 SELECT
   generate_series(candles.start_time, (candles.start_time+60)) AS time,
-  max(candles.high) AS high,
-  max(candles.low) AS low,
-  max(candles.open) AS open,
-  max(candles.close) AS close
+  max(candles.high) AS c_high,
+  max(candles.low) AS c_low,
+  max(candles.open) AS c_open,
+  max(candles.close) AS c_close
 FROM
   candles
+GROUP BY
+  time
+ORDER BY
+  time
+
+WITH averages AS (
+  SELECT
+    DISTINCT(floor(event_time/1000/60)::int * 60) AS time,
+    avg(price) OVER (PARTITION BY floor(event_time/1000/60)::int * 60) AS price,
+  FROM
+    binance_trade
+  WHERE
+    event_time/1000 >= $__unixEpochFrom()
+  AND
+    event_time/1000 <= $__unixEpochTo()
+),
+bollinger_bands_21_2 AS (
+  SELECT
+    time AS time,
+    avg(price) OVER (ORDER BY time ROWS 20 PRECEDING) AS average,
+    avg(price) OVER (ORDER BY time ROWS 20 PRECEDING)
+      + (stddev(price) OVER (ORDER BY time ROWS 20 PRECEDING)*2) AS high,
+    avg(price) OVER (ORDER BY time ROWS 20 PRECEDING)
+      - (stddev(price) OVER (ORDER BY time ROWS 20 PRECEDING)*2) AS low
+  FROM
+    averages
+)
+SELECT
+  bollinger_bands_21_2.time AS time,
+  max(bollinger_bands_21_2.average) AS bb_average,
+  max(bollinger_bands_21_2.high) AS bb_high,
+  max(bollinger_bands_21_2.low) AS bb_low
+FROM
+  bollinger_bands_21_2
 GROUP BY
   time
 ORDER BY
